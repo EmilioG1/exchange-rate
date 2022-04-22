c80d53e4e687845ed8359d552d2bd8d6f1719df2
Emilio Gonzales <emiliog1184@gmail.com>
2022-04-22 08:54:45 -0700

    add api key and api call class

---
+++ src/exchange.js
@@ -0,0 +1,15 @@
+export class ApiCall {
+  static get(url) {
+    return fetch(url)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        console.log(error);
+        return error;
+      });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { ApiCall } from './../src/exchange.js';
 
-$("").submit(function (event) {
+
+async function getExchange(money) {
+  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
+  const response = await ApiCall.get(url);
+}
+
+$("#submit").submit(function (event) {
   event.preventDefault();
+  
+
 });
