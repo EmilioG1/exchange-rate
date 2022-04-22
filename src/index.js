e4e60dcb5e88ba4ffd2933393cb926f2ef7a9399
Emilio Gonzales <emiliog1184@gmail.com>
2022-04-22 12:37:57 -0700

    fix getEx function and ui output

--- src/index.js
+++ src/index.js
@@ -16,29 +16,15 @@ function error(element, response) {
   }
 }
 
-function rate(num, value) {
-  if (value === 'EUR') {
-    return num * 0.9213;
-  } else if (value === 'GBP') {
-    return num * 0.7672;
-  } else if (value === 'JPY') {
-    return num * 128.3156;
-  } else if (value === 'AMD') {
-    return num * 467.7888;
-  } else if (value === 'CHF') {
-    return num * 0.9519;
-  } else {
-    return error('.output', "not a currency");
-  }
-}
-
-async function getExchange(currency) {
-  const url = `https://v6.exchangerate-api.com/v6/9e9e75e88278f7b0de98ee1a/latest/${currency}`;
+async function getExchange(num, currency) {
+  const url = `https://v6.exchangerate-api.com/v6/9e9e75e88278f7b0de98ee1a/pair/USD/${currency}/${num}`;
   const response = await ApiCall.get(url);
   const withError = error($('.output'), response);
+  console.log(response.conversion_result);
   if (!withError) {
-    const rateURL = response.url;
-    const output = rate(rateURL);
+    return await response.conversion_result;
+  } else {
+    alert(response);
   }
 }
 
@@ -46,7 +32,8 @@ $("#rate").submit(async function (event) {
   event.preventDefault();
   let input1 = parseInt($('#input1').val());
   let input2 = $('#input2').val();
-  input2.toLowerCase();
   console.log(input1, input2);
-  const output = getExchange(input2);
+  const output = await getExchange(input1, input2);
+  console.log(output);
+  $('.output').text(`That's ${output} dollars`);
 });
